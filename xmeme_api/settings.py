2819ea60d5f4c51c3502b894bf8a7d88247b5a76
shikharsun <shikhar21121999@gmail.com>
2021-02-12 22:12:34 +0530

    deploy changes

--- xmeme_api/settings.py
+++ xmeme_api/settings.py
@@ -142,9 +142,9 @@ STATICFILES_DIRS = (
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CORS_ORIGIN_ALL = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+# ]
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
